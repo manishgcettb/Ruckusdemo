60c209807f080d81ded0c7ca19855e45984f8640
manishgcettb <manish15.kumar@aricent.com>
2015-01-20 11:03:36 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,25 @@
+public class Demo {
+
+	private int firstnumber;
+	private int lastnumber;
+	
+	public Test1(int firstnumber,int lastnumber){
+		this.firstnumber = firstnumber;
+		this.lastnumber = lastnumber;
+	}
+	public int addNumbers(){
+		return firstnumber + lastnumber;
+	}
+	
+	public int multiplyNumbers(){
+		return firstnumber*lastnumber;
+	}
+	
+	public int devideNumbers(){
+		return firstnumber/lastnumber;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Main Class ");
+	}
+}
